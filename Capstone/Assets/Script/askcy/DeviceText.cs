519910a9107f93d77256a7bf70e37eb4b62789c5
oirbeboq <130990969+oirbeboq@users.noreply.github.com>
2026-03-10 14:33:54 -0700

    Revert "Merge branch 'main' of https://github.com/Pigeon-hcy/Capstone"
    This reverts commit dc23f4c26e9a19f9430cf80d146a2092e34315bb, reversing
    changes made to a755d936c6ba9fee329436f7cc07222993b75313.

--- Capstone/Assets/Script/askcy/DeviceText.cs
+++ Capstone/Assets/Script/askcy/DeviceText.cs
@@ -5,9 +5,6 @@ using SkateGame;
 
 public class DeviceText : MonoBehaviour, ICanRegisterEvent, IBelongToArchitecture
 {
-    // 缓存当前输入设备类型，供其他脚本（如 TMPTextRevealBounce）使用
-    public static InputDeviceType LastDeviceType { get; private set; } = InputDeviceType.KeyboardMouse;
-
     [SerializeField] TMP_Text text;
 
     void Awake()
@@ -27,21 +24,6 @@ public class DeviceText : MonoBehaviour, ICanRegisterEvent, IBelongToArchitectur
 
     void OnDeviceSwitched(InputDeviceSwitchedEvent e)
     {
-        // 更新当前设备类型缓存
-        LastDeviceType = e.DeviceType;
-
-        // 根据设备类型选择原始文本
-        string raw = e.DeviceType == InputDeviceType.Gamepad ? gamepadText : keyboardMouseText;
-
-        // 替换按键占位符（例如 {Jump}）
-        string jumpKey = e.DeviceType == InputDeviceType.Gamepad ? "A" : "Space";
-        string processed = raw.Replace("{Jump}", jumpKey);
-
-        // 将自定义的 {vertexp} 标签转换为 Febucci 使用的 [vertexp] 标签
-        processed = processed
-            .Replace("{vertexp}", "[vertexp]")
-            .Replace("{/vertexp}", "[/vertexp]");
-
-        text.text = processed;
+        text.text = e.DeviceType == InputDeviceType.Gamepad ? gamepadText : keyboardMouseText;
     }
 }
