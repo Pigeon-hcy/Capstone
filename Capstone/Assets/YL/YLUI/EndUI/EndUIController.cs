cac9b1c16007e85bba0aa5df652a7c4b9bd4eaf2
Youngjun Lee <131013184+yuya2358@users.noreply.github.com>
2026-04-05 19:55:11 -0400

    Developing EndUI

--- Capstone/Assets/YL/YLUI/EndUI/EndUIController.cs
+++ Capstone/Assets/YL/YLUI/EndUI/EndUIController.cs
@@ -1,16 +1,86 @@
+using System.Collections;
+using SkateGame;
 using UnityEngine;
 
 public class EndUIController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] RectTransform shutterTrans;
+    [SerializeField] Animator shutterAnim;
+    [SerializeField] EndUICollider endUICollider;
+    [SerializeField] GameObject navigator;
+    
+    private void Start()
     {
-        
+        endUICollider.OnTriggerEntered += OnTargetHit;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTargetHit(Collider2D other)
     {
-        
+        StartCoroutine(StartEndAnimation());
     }
+
+    public void EndSequence_A()
+    {
+        StartCoroutine(StartEndAnimation());
+    }
+
+    public void EndSequence_B(Collider2D other)
+    {
+        Rigidbody2D rb = other.attachedRigidbody;
+        if (rb == null) rb = other.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+        }
+        
+        PlayerController playerController = other.GetComponent<PlayerController>();
+        if (playerController != null)
+        {
+            playerController.disableInput = true;
+            playerController.rb.linearVelocity = Vector2.zero;
+        }
+    }
+
+    public void EndSequence_C()
+    {
+        navigator.SetActive(true);
+        GameStateController.Instance.EnterUIPause();
+    }
+
+    IEnumerator StartEndAnimation()
+    {
+        shutterTrans.gameObject.SetActive(true);
+        shutterAnim.Play("anim_EndUI", 0, 0);
+        shutterTrans.localScale = Vector3.one * 1.2f;
+        //shutterTrans.localRotation = Quaternion.Euler(0, 0, 15f);
+        
+        float lerpSpeed = 1f;
+
+        while (shutterTrans.localScale.x > 1.001f)
+        {
+            shutterTrans.localScale = Vector3.Lerp(
+                shutterTrans.localScale, 
+                Vector3.one, 
+                Time.deltaTime * lerpSpeed
+            );
+
+            /*
+            shutterTrans.localRotation = Quaternion.Lerp(
+                shutterTrans.localRotation, 
+                Quaternion.identity, 
+                Time.deltaTime * lerpSpeed
+            );
+            */
+
+            yield return null;
+        }
+        
+        shutterTrans.localScale = Vector3.one;
+        shutterTrans.localRotation = Quaternion.identity;
+
+        shutterTrans.localScale = Vector3.one;
+    }
+
+
 }
